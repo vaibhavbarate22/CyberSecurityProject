53f481b269667d19644acc849195004c8b8e4111
vaibhavbarate <vaibhavbarate7@gmail.com>
2024-11-19 16:14:37 +0530

    Version 0.1

--- cybersecurity project python/BasicFirewall.py
+++ cybersecurity project python/BasicFirewall.py
@@ -1,10 +1,11 @@
 import random
 import logging
 
-logging.basicConfig(filename="BasicFirewall.log" ,level=logging.INFO ,format='%(asctime)s - %(message)s')
+
+logging.basicConfig(filename="BasicFirewall.log", level=logging.INFO, format='%(asctime)s - %(message)s')
+
 def LogEvent(message):
     logging.info(message)
-
 
 def TrackConnections(Ip, AllowedConnections):
     if Ip not in AllowedConnections:
@@ -18,10 +19,8 @@ def CheckFirewallRule(Ip, Rules):
     for RuleIp, Action in Rules.items():
         if Ip == RuleIp:
             return Action
-        if not Rules :
-            return "Allow"
+    return "Allow"
 
-    
 def main():
     FirewallRules = {
         "192.168.1.1": "block",
@@ -43,12 +42,12 @@ def main():
 
         LogMessage = f"IP: {IpAddress} RND: {RandomNumber} Action: {Action}"
 
-
         if Action == "block":
             BlockedIp.append(f"Ip : {IpAddress}  RND : {RandomNumber}")
             LogEvent(f"Blocked {LogMessage}")
         else:
-            Action == TrackConnections(IpAddress,AllowedConnections)
+           
+            Action = TrackConnections(IpAddress, AllowedConnections)
             AllowedIp.append(f"Ip : {IpAddress}  RND : {RandomNumber}")
             LogEvent(f"Allowed {LogMessage}")
 
@@ -56,7 +55,7 @@ def main():
     print("\n".join(BlockedIp) if BlockedIp else "No IPs blocked.")
 
     print("\n<--- Allowed IP --->")
-    print("\n".join(AllowedIp)if AllowedIp else "No IPs allowed.")
+    print("\n".join(AllowedIp) if AllowedIp else "No IPs allowed.")
 
 if __name__ == "__main__":
     main()
